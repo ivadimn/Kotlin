973757e75e5de67ff2c85d877168a8fb7b5b4de2
vadim <ivadimn@mail.ru>
2020-09-23 17:45:38 +0300

    lesson03 update

--- Lesson03/src/main/kotlin/calc/Calc.kt
+++ Lesson03/src/main/kotlin/calc/Calc.kt
@@ -1,5 +1,7 @@
 package calc
 
+import java.lang.IllegalArgumentException
+
 fun Any.print() : String  =
     when (this) {
         is String ->  "String: ${this}"
@@ -22,21 +24,45 @@ fun List<Int>.sum() : Int {
 }
 
 fun isValidId(id : String) : Boolean {
-    if (id.length <= 4) return false
-    val fch = id.first()
-    if ((fch != '_') or (!fch.isLetter()))  return false
-    id.contains("""[]""")
+    val regex = """[_a-zA-z]{1}[a-zA-Z0-9_]{3,}"""
+    return id.matches(regex.toRegex())
 }
 
-fun main() {
+interface Animal {
+    fun say()
+}
 
-    val s = "lkd;lk;ks;"
-    println(s.print())
-    println(255.print())
-    println(true.print())
-    println(fib(2))
-    println(fib(3))
-    println(fib(12))
-    println(listOf(1, 2, 3, 4, 5, 6, 7).sum())
+class Dog : Animal {
+    override fun say() {
+        println("Собака гавкает")
+    }
+}
 
+class Goat : Animal {
+    override fun say() {
+        println("Козёл блеет")
+    }
+}
+
+class Cow : Animal {
+    override fun say() {
+        println("Корова мычит")
+    }
+}
+
+interface Expr
+class Num(val value: Int) : Expr
+class Sum(val left: Expr, val right : Expr) : Expr
+
+fun eval(e: Expr) : Int =
+    when(e) {
+        is Num -> e.value
+        is Sum -> eval(e.left) + eval(e.right)
+        else -> throw IllegalArgumentException("Invalid expression")
+    }
+fun  main() {
+    val farm = listOf<Animal>(Dog(), Goat(), Cow())
+    farm.forEach { it.say() }
+
+    println(eval(Sum( Sum( Num(2), Num(7)), Num(10))))
 }
