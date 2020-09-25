21689220f6a348b06742468d682e8dd74291f09a
vadim <ivadimn@mail.ru>
2020-09-25 06:10:49 +0300

    new version

--- Lesson03/src/main/kotlin/over/Over.kt
+++ Lesson03/src/main/kotlin/over/Over.kt
@@ -1,2 +1,31 @@
 package over
 
+enum class Color(
+    val r: Int,
+    val g: Int,
+    val b: Int
+) {
+    RED(255, 0, 0),
+    GREEN(0, 255, 0),
+    BLUE(0, 0, 255);
+
+    override fun toString(): String =
+        when(this) {
+            RED -> "Red"
+            GREEN -> "Green"
+            BLUE -> "Blue"
+        }
+}
+
+
+
+fun main() {
+    val cs = mutableListOf<Color>(Color.RED, Color.BLUE, Color.GREEN, Color.RED, Color.RED)
+    val list = mutableListOf<Color>()
+    cs.forEach { list.add(it) }
+    cs.removeAt(0)
+    println(cs.toString())
+    //println(list.toString())
+
+}
+
