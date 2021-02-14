b43fe5667bbb21640c6856bf7938d8a8d782d93e
vadim <ivadimn@mail.ru>
2021-02-14 21:35:01 +0300

    collections

--- Collections82/src/main/kotlin/collections/Collctions.kt
+++ Collections82/src/main/kotlin/collections/Collctions.kt
@@ -1,2 +1,28 @@
 package collections
 
+data class Repo(
+    val id : Int,
+    val name : String,
+    var starred : Boolean = false
+)
+
+val raw = listOf(
+    Repo(1, "repo_1"),
+    Repo(2, "repo_2"),
+    Repo(3, "repo_3"),
+    Repo(4, "repo_4"),
+    Repo(5, "repo_5")
+)
+
+val starred = listOf(
+    Repo(1, "repo_1"),
+    Repo(3, "repo_3"),
+    Repo(5, "repo_5")
+)
+
+
+fun main() {
+    val stars = raw.intersect(starred)
+    raw.forEach { repo -> if (repo in starred) repo.starred = true }
+    println(raw)
+}
